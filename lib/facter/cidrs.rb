8229612752fe999967c2da8f938c8863a15412e7
Simon Lauger <simon@lauger.de>
2024-05-28 11:39:07 +0200

    remove cidrs

--- lib/facter/cidrs.rb
+++ lib/facter/cidrs.rb
@@ -6,7 +6,7 @@ Facter.add("cidrs") do
     
     interfaces.each do |interface_name, interface_params|
       if interface_params['physical']
-        if 'netmask' in interface_params
+        if interface_params.has_key?('netmask')
           octets = interface_params['netmask'].split('.').map(&:to_i)
           cidr = octets.map { |octet| octet.to_s(2).count('1') }.sum
           cidrs[interface_name] = interface_params["network"] + "/" + cidr.to_s
