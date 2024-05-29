9c170219a19503bcf5e3810d300bd531eccc56c7
Simon Lauger <simon@lauger.de>
2024-05-29 16:09:00 +0200

    fix: bring back cidrs fact

--- lib/facter/cidrs.rb
+++ lib/facter/cidrs.rb
@@ -3,10 +3,9 @@ Facter.add("cidrs") do
     cidrs = {}
 
     interfaces = Facter.value('networking.interfaces')
-    
     interfaces.each do |interface_name, interface_params|
       if interface_params['physical']
-        if interface_params.has_key?('netmask')
+        if interface_params.key?('network') and interface_params.key?('netmask')
           octets = interface_params['netmask'].split('.').map(&:to_i)
           cidr = octets.map { |octet| octet.to_s(2).count('1') }.sum
           cidrs[interface_name] = interface_params["network"] + "/" + cidr.to_s
